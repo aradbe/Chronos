2f5a4acccc0675456a4007a2fa7cee317c909b52
aradbe <aradbearad@gmail.com>
2026-08-24 10:16:44 +0300

    fix: align pixel avatar layers

--- client/src/components/avatar/PixelAvatar.jsx
+++ client/src/components/avatar/PixelAvatar.jsx
@@ -1,7 +1,7 @@
 import { AVATAR_DEFAULTS } from "./avatarOptions";
 import "./PixelAvatar.css";
 
-const ASSET_ROOT = "/assets/avatars/sprite-v2";
+const ASSET_ROOT = "/assets/avatars/sprite-v2/normalized";
 
 export function PixelAvatar({ avatar = {}, size = "large", label = "Your character" }) {
   const value = { ...AVATAR_DEFAULTS, ...avatar };
@@ -15,12 +15,7 @@ export function PixelAvatar({ avatar = {}, size = "large", label = "Your charact
       <div className="pixel-avatar__stage">
         <img
           alt=""
-          className={`pixel-avatar__layer pixel-avatar__body pixel-avatar__body--torso pixel-avatar__body--${value.skin}`}
-          src={`${ASSET_ROOT}/body.png`}
-        />
-        <img
-          alt=""
-          className={`pixel-avatar__layer pixel-avatar__body pixel-avatar__body--head pixel-avatar__body--${value.skin}`}
+          className={`pixel-avatar__layer pixel-avatar__body pixel-avatar__body--${value.skin}`}
           src={`${ASSET_ROOT}/body.png`}
         />
         <img
@@ -28,11 +23,11 @@ export function PixelAvatar({ avatar = {}, size = "large", label = "Your charact
           className="pixel-avatar__layer pixel-avatar__outfit"
           src={`${ASSET_ROOT}/outfit-${value.outfit}.png`}
         />
-        <div className={`pixel-avatar__face pixel-avatar__face--${value.face}`}>
-          <i className="pixel-avatar__eye pixel-avatar__eye--left" />
-          <i className="pixel-avatar__eye pixel-avatar__eye--right" />
-          <i className="pixel-avatar__mouth" />
-        </div>
+        <img
+          alt=""
+          className="pixel-avatar__layer pixel-avatar__face"
+          src={`${ASSET_ROOT}/face-${value.face}.png`}
+        />
         <img
           alt=""
           className={`pixel-avatar__layer pixel-avatar__hair pixel-avatar__hair--${value.hair} pixel-avatar__hair-color--${value.hairColor}`}
